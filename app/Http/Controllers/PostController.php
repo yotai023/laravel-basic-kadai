e79c500c500f826af25a9407161d5f27f4c1f44d
yotai023 <yotai023333@gmail.com>
2024-10-21 23:49:50 +0900

    課題提出

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index(){
-        return view('posts.index');
+        $posts = DB::table('posts')->get();
+
+        return view('posts.index', compact('posts'));
     }
 }
